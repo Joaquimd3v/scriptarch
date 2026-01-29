7fe08593ff7f46ce991bb3fcaa287a8d872f67f9
Kauã Joaquim <joaquiimkaua@gmail.com>
2026-01-29 13:11:44 -0300

    Simplify script to execute dotfiles installation

--- file.sh
+++ file.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-# Script para baixar e executar hyprland-dotfiles-stable.dotinst
-
-set -e
-
-URL="https://raw.githubusercontent.com/mylinuxforwork/dotfiles/main/hyprland-dotfiles-stable.dotinst"
-FILE="hyprland-dotfiles-stable.dotinst"
-
-echo "Baixando dotfiles..."
-curl -fsSL "$URL" -o "$FILE"
-
-echo "Dando permissão de execução..."
-chmod +x "$FILE"
-
-echo "Executando o instalador..."
-./"$FILE"
-
-echo "Concluído!"
+bash <(curl -fsSL https://raw.githubusercontent.com/mylinuxforwork/dotfiles/main/hyprland-dotfiles-stable.dotinst)
